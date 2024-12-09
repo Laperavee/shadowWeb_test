3667c46fef1b7998e2912589a38880bdee9a9642
Miniard <114595598+Miniard@users.noreply.github.com>
2024-12-09 20:05:31 +0100

    dqsds

--- vite.config.cjs
+++ vite.config.cjs
@@ -3,9 +3,9 @@ const react = require('@vitejs/plugin-react')
 
 module.exports = defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    host: true,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   }
 }) 
