10eb9b2b7b1ea5b71fe1723f1d20242ac86e331a
laperavee <tomlebonpro@gmail.com>
2025-04-22 17:01:39 +0200

    fix

--- src/pages/ShadowFun.jsx
+++ src/pages/ShadowFun.jsx
@@ -81,7 +81,7 @@ const NetworkSelector = ({ selectedChain, onChange }) => {
       </button>
 
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 rounded-lg bg-black/20 backdrop-blur-sm border border-white/10">
+        <div className="absolute right-0 mt-2 min-w-[160px] rounded-lg bg-black/20 backdrop-blur-sm border border-white/10">
           {Object.entries(NETWORKS).map(([key, network]) => (
             <button
               key={key}
