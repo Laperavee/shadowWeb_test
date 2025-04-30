a96648554bbfc537f66f0feeaf5366f3c6e430f7
laperavee <tomlebonpro@gmail.com>
2025-04-30 20:11:58 +0200

    commit

--- src/config/contracts.js
+++ src/config/contracts.js
@@ -1,7 +1,7 @@
 // Créer un nouveau fichier pour la configuration des contrats
 export const CONTRACTS = {
     AVAX: {
-      SHADOW_ADDRESS: "0xEB195Cd5554310F9B107C017c52516Fe5FE726B1",
+      SHADOW_ADDRESS: "0x6E63C1B744afdAa503F6744412cBB5500b59e32B",
     },
     BASE: {
       SHADOW_ADDRESS: "0x9cb0607cc38f79cb5ed30a96f5cf5b3c84da8127",
