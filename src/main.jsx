7109454b56f1dbfd3aca3b49aac9b343c31ebe6c
laperavee <tomlebonpro@gmail.com>
2025-04-30 17:24:30 +0200

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { NetworkProvider } from './context/NetworkContext'
+import { SoundProvider } from './context/SoundContext'
+import { NotificationProvider } from './context/NotificationContext'
+import { WalletProvider } from './context/WalletContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <NetworkProvider>
+      <SoundProvider>
+        <NotificationProvider>
+          <WalletProvider>
+            <App />
+          </WalletProvider>
+        </NotificationProvider>
+      </SoundProvider>
+    </NetworkProvider>
   </React.StrictMode>,
 ) 
